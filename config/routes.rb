7075e791ec44170c467a1d2dab865d2984b54b0b
Oubi12 <robiehbels@gmail.com>
2023-11-11 16:05:16 +0800

    feature 3: client homepage and session pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,20 @@ Rails.application.routes.draw do
     namespace :admin do
       root "home#index"
     end
-
     devise_for :users, as: :admin, path: 'admin', controllers: {
       sessions: 'admin/users/sessions'
     }, skip: [:registrations]
   end
 
-
   constraints(ClientDomainConstraint.new) do
     namespace :client do
       root "home#index"
-    end
 
+    end
     devise_for :users, as: :client, path: 'client', controllers: {
-      sessions: 'client/users/sessions'
+      sessions: 'client/users/sessions',
+      registrations: 'client/users/registrations'
     }
   end
+  resources :menu , only: :index
 end
