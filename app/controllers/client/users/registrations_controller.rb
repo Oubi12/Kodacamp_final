ba87fb2502c6420aee90941397cfc10d4e0cc6f0
Oubi12 <robiehbels@gmail.com>
2023-11-13 20:55:31 +0800

    feature 7: add copy button in invite page and add parent id column in user

--- app/controllers/client/users/registrations_controller.rb
+++ app/controllers/client/users/registrations_controller.rb
@@ -5,14 +5,24 @@ class Client::Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+    if params[:promoter].present?
+      cookies[:promoter] = params[:promoter]
+    end
+  end
+
+  def create
+    super do |resource|
+      if (promoter_email = cookies.delete(:promoter)).present?
+        promoter = User.find_by(email: promoter_email)
+        resource.parent = promoter if promoter
+        resource.save
+      end
+    end
+  end
+
 
   # GET /resource/edit
   # def edit
@@ -41,9 +51,9 @@ class Client::Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :parent_id])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
---
+++ db/migrate/20231113125247_add_parent_id_to_users.rb
@@ -0,0 +1,7 @@
+class AddParentIdToUsers < ActiveRecord::Migration[7.0]
+  def change
+    def change
+      add_column :users, :parent_id, :integer
+    end
+  end
+end
