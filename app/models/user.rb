1a37d594458b298aa5ff2bd61fb497ae67ac19bc
Oubi12 <robiehbels@gmail.com>
2023-11-14 14:24:57 +0800

    feature 8: increment children members

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
     countries: [:ph]
   }
   mount_uploader :image, ImageUploader
+
+  belongs_to :parent, class_name: 'User', optional: true, counter_cache: :children_members
+  has_many :children, class_name: 'User', foreign_key: 'parent_id'
+
   has_many :addresses
 
   enum role: { client: 0, admin: 1 }
