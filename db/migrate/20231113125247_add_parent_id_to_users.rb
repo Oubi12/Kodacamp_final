d310b9b61357c0c01c7e3652dda56c875583676f
Oubi12 <robiehbels@gmail.com>
2023-11-14 14:46:01 +0800

    fix my schema

--- db/migrate/20231113125247_add_parent_id_to_users.rb
+++ db/migrate/20231113125247_add_parent_id_to_users.rb
@@ -1,7 +1,5 @@
 class AddParentIdToUsers < ActiveRecord::Migration[7.0]
   def change
-    def change
-      add_column :users, :parent_id, :integer
-    end
+    add_column :users, :parent_id, :integer
   end
 end
