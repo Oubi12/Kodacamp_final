95cf30ffb6524716aca90e7d72549fa47d77dac1
Oubi12 <robiehbels@gmail.com>
2023-11-30 20:15:30 +0800

    feature 23: Profile add order and lotteries history

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,8 @@ Rails.application.routes.draw do
     resources 'client/tickets', as: 'submit_tickets', path: 'submit_tickets', only: [:create]
     resources 'client/shop', as: 'shop', path: 'shop', only: [:index, :show]
     resources 'client/orders', as: 'purchase_orders', path: 'purchase_orders', only: [:create]
+    resources 'client/me/orders', as: 'order_history', path: 'me/orders', only: :index
+    resources 'client/me/lotteries', as: 'lottery_history', path: 'me/lotteries', only: :index
   end
 
   namespace :api do
